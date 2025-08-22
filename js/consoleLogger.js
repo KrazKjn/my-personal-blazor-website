a8150759a2edfe85dac79220d8815eb616c90a29
krazkjn <krazkjn@msn.com>
2025-08-22 13:25:29 -0500

    - Fixed NotFound - Added logic to find closest or list the items found by page name - Added browser console logging

---
+++ js/consoleLogger.js
@@ -0,0 +1,11 @@
+window.consoleLogger = {
+    log: function (message) {
+        console.log(message);
+    },
+    warn: function (message) {
+        console.warn(message);
+    },
+    error: function (message) {
+        console.error(message);
+    }
+};
